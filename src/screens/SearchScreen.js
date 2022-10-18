7a235ff202e32113e818f46ae9c7f5d772960d70
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-10-19 02:00:17 +0900

    Constraining View Elements

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -18,7 +18,7 @@ const SearchScreen = () => {
   };
 
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <SearchBar
         term={term}
         onTermChange={setTerm} // same as {(newTerm) => setTerm(newTerm)}
